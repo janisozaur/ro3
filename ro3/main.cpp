66b4fc178a9ef7237c19d618287fcc1b84291db0
Michał Janiszewski <janisozaur@gmail.com>
2011-12-27 00:03:22 +0100

    add simple tests

--- ro3/main.cpp
+++ ro3/main.cpp
@@ -1,8 +1,25 @@
+#include "GaussianFilter.h"
+#include "EdgeDetector.h"
+
 #include <QtCore/QCoreApplication>
+#include <QImage>
+#include <QStringList>
+
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
 	
-	return a.exec();
+	const qreal size = 5;
+	const qreal stdDev = GaussianFilter::devForSize(size);
+	qDebug() << "stddev for size" << size << "equals" << stdDev;
+	GaussianFilter gf(size, stdDev);
+	QImage img(a.arguments().at(1));
+	QImage result = gf.gaussianBlur(img);
+	result.save(a.arguments().at(1) + "_gaussian.png");
+	EdgeDetector ed;
+	QImage edges = ed.detect(img);
+	edges.save(a.arguments().at(1) + "_edges.png");
+	return 0;
 }
